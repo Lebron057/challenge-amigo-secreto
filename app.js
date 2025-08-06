e04931c39b202da9aefd7f087267d04210af62ae
Lebron057 <iagoamorim057@gmail.com>
2025-08-06 11:30:07 -0300

    criar o array e implementar função de agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigo = [];
+
+function adicionarAmigo() {
+    let nome = document.getElementById('amigo').value.trim();
+
+    if (nome == '') {
+        alert('Campo do nome vazio, insira um nome!');
+    } else {
+        amigo.push(nome);
+        console.log(amigo);        
+    }
+
+    limparCampo();
+}
+
+function limparCampo() {
+    nome = document.getElementById('amigo');
+    nome.value = '';
+}
