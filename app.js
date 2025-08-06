403b410ac077adf6e9040237bcd93d8190c3c9a0
Lebron057 <iagoamorim057@gmail.com>
2025-08-06 17:37:03 -0300

    função sortearAmigo funcionando mas sem criterios

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 let amigo = [];
+numeroAmigos = 0;
 
 function adicionarAmigo() {
-    let nome = document.getElementById('amigo').value.trim();
+    let nome = document.getElementById('amigo').value.trim(); //trim() tira os espaços
 
     if (nome == '') {
         alert('Campo do nome vazio, insira um nome!');
     } else {
         amigo.push(nome);
-        console.log(amigo);        
+        numeroAmigos++;
+        console.log(amigo);  
+        console.log('numeroAmigos =', numeroAmigos);      
     }
 
     document.getElementById('listaAmigos').innerHTML = '';
 
-    let cont = 0;
-
-    while (cont < amigo.length) {
-        exibirNaTela('listaAmigos', amigo[cont]);
-        cont++;
+    //mostra na tela a lista de amigos
+    for (let i = 0; i < amigo.length; i++) {
+        exibirListaTela('listaAmigos', amigo[i]);
     }
 
     limparCampo();
@@ -27,7 +28,22 @@ function limparCampo() {
     nome.value = '';
 }
 
-function exibirNaTela(id, texto) {
+function exibirListaTela(id, texto) {
     let campo = document.getElementById(id);
     campo.innerHTML += `<p>${texto}</p>`;
+}
+
+function exibirNaTela(id, texto) {
+    let campo = document.getElementById(id);
+    campo.innerHTML = texto;
+}
+
+function sortearAmigo() {
+    if (numeroAmigos < 1) {
+        alert('Não há nomes para sortear!');
+    } else {
+        let numeroAleatorio = parseInt(Math.random() * numeroAmigos);
+        console.log('numeroAleatorio =', numeroAleatorio);
+        exibirNaTela('resultado', `Seu amigo secreto é ${amigo[numeroAleatorio]}!`);
+    }
 }
